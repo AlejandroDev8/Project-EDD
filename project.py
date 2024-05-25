5514d7b6e046fe0d5c1bc4a1725a53ea9109a192
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-05-25 17:43:11 -0600

    refactor: implement insertion sort algorithm in project.py

--- project.py
+++ project.py
@@ -8,6 +8,15 @@ def bubble_sort(arr):
       if arr[j] > arr[j+1]:
         arr[j], arr[j+1] = arr[j+1], arr[j]
 
+def insertion_sort(arr):
+  for i in range(1, len(arr)):
+    key = arr[i]
+    j = i-1
+    while j >= 0 and key < arr[j]:
+      arr[j + 1] = arr[j]
+      j -= 1
+    arr[j + 1] = key
+
 def generate_random_list(n):
   return [random.randint(0, 50) for _ in range(n)]
 
